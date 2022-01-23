30db802584a15e77d452305bde51e98c009366d1
Steven Slocum <stevesl23@hotmail.com>
2022-01-23 10:00:45 -0600

    created initial About Me Component and Nav component

---
+++ src/Components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+        <div>
+            <h2>About Me Test</h2>
+            <section>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut consectetur nam aliquam, magni tempore iusto dolor a voluptate ullam vel dignissimos commodi ex nisi debitis fuga? Dolorem vel assumenda doloremque?</section>
+        </div>
+    );
+}
+
+export default AboutMe;
