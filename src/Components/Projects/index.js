3e7dfe45b79d4ce283acf7fe9410ab28b2e5f805
Steven Slocum <stevesl23@hotmail.com>
2022-01-23 21:26:18 -0600

    added margins around the rows to add spacing

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import { Card, Container, Row, Col } from 'react-bootstrap';
 
 function Projects() {
     return (
         <Container>
-            <Row>
+            <Row className='m-4'>
                 <Col>
                     <Card className="bg-dark text-white">
                         <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
@@ -32,10 +33,46 @@ function Projects() {
                     </Card>
                 </Col>
             </Row>
-            <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
+            <Row className='m-4'>
+                <Col className='m-auto'>
+                <Card className="bg-dark text-white">
+                        <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                    </Card>
+                </Col>
+                <Col>
+                <Card className="bg-dark text-white">
+                        <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                    </Card>
+                </Col>
+                <Col>
+                <Card className="bg-dark text-white">
+                        <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                    </Card>
+                </Col>
             </Row>
         </Container>
     );
