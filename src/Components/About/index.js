271312ec7a966ad24754cd6bc177b19dff44562d
Steven Slocum <stevesl23@hotmail.com>
2022-01-24 00:18:06 -0600

    added styling to about me text

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -10,8 +10,8 @@ function AboutMe() {
             <img className='mw-50 mh-50' src={avatar}></img>
             </div>
             <div className='d-flex flex-column align-items-center mt-4'>
-            <section className='mx-5 mb-4'>Hello! My name is Steven Slocum and I am a Full-Stack Engineer. I am currently based out of Minneapolis, MN. </section>
-            <section className='mx-5'>I have a passion for creating things! I create webpages, video-games, and music in a band called "The Cellar Door". I also have an affinty for playing video games at a semi-pro level. I have competed at major tournaments such as "Combo-Breaker" and "Frosty Faustings". My goal is to work in a development role for companies creating databases, webpages, and eventually game development. Please browse my below work and reach out to me if you think I would be a good fit for your team!</section>
+            <section className='mx-5 mb-4 font-weight-bold'>Hello! My name is Steven Slocum and I am a Full-Stack Engineer. I am currently based out of Minneapolis, MN. </section>
+            <section className='mx-5 font-weight-bold'>I have a passion for creating things! I create webpages, video-games, and music in a band called "The Cellar Door". I also have an affinty for playing video games at a semi-pro level. I have competed at major tournaments such as "Combo-Breaker" and "Frosty Faustings". My goal is to work in a development role for companies creating databases, webpages, and eventually game development. Please browse my below work and reach out to me if you think I would be a good fit for your team!</section>
             </div>
         </Container>
     );
