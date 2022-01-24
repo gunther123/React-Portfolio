38da6ee650874ddfacaad07c98897b6e315b83cd
Steven Slocum <stevesl23@hotmail.com>
2022-01-23 20:31:17 -0600

    created placeholder layout for project cards

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 
 function Projects() {
     return (
-        <div>
-            <section> Test 2! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut consectetur nam aliquam, magni tempore iusto dolor a voluptate ullam vel dignissimos commodi ex nisi debitis fuga? Dolorem vel assumenda doloremque?</section>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Card className="bg-dark text-white">
+                        <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
+                            <Card.ImgOverlay>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className="bg-dark text-white">
+                            <Card.Img src="https://place-hold.it/350x350" alt="Card image" />
+                                <Card.ImgOverlay>
+                                <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                            </Card.ImgOverlay>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
     );
 }
 
